c63c168c163c34eeecaeee24a45567ce1b438f61
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-06 13:29:45 -0500

    cambios en la configuracion de la base de datos

--- application/config/config.php
+++ application/config/config.php
@@ -378,7 +378,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'HPE_promo_made_simple';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
