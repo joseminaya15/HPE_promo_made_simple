73f493c79bf460c5c6dd9021fbda3bcf3d27f7c0
joseminaya15 <jose.minayac15@gmail.com>
2018-03-07 17:08:53 -0500

    login

---
+++ application/views/v_login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+        <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="description"            content="Registro de Oportunidades DCN">
+        <meta name="keywords"               content="Registro de Oportunidades DCN">
+        <meta name="robots"                 content="Index,Follow">
+        <meta name="date"                   content="Febrero 15, 2018"/>
+        <meta name="language"               content="es">
+        <meta name="theme-color"            content="#000000">
+    	<title>HPE promo made simple</title>
+    	<link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.png">
+    	<link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>font-awesome.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>roboto.css?v=<?php echo time();?>">
+    	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
+    	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
+    </head>
+    <body>
+        <section>
+            <div class="center-login">
+                <div class="mdl-card mdl-card-login">
+                    <div class="mdl-card__title">
+                        <img src="<?php echo RUTA_IMG?>logo/logo_login.png">
+                    </div>
+                    <div class="mdl-card__supporting-text">
+                        <div class="col-xs-12">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="text" id="usuario">
+                                <label class="mdl-textfield__label" for="usuario">Usuario</label>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="text" id="password">
+                                <label class="mdl-textfield__label" for="password">Contrase&ntilde;a</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mdl-card__actions">
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Ingresar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>login.js?v=<?php echo time();?>"></script>
+    </body>
+</html>
