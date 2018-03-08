9f1cab24ab963a791418b2b83a78de47d7422e70
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-08 15:40:49 -0500

    cards de forma dinamica

--- application/controllers/Promo.php
+++ application/controllers/Promo.php
@@ -6,6 +6,7 @@ class Promo extends CI_Controller {
 	function __construct() {
         parent::__construct();
         $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
+        $this->load->model('M_solicitud');
         $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
         $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
@@ -15,6 +16,69 @@ class Promo extends CI_Controller {
 	public function index()
 	{
 		$data['nombre'] = 'hola';
+        $promociones = $this->M_solicitud->getPromociones();
+        $html = '';
+        $cont = 1;
+        $exp = 1;
+        $datos_sales = "";
+        $datos_bu = "";
+        foreach (explode(",", $promociones[0]->Contactos_sales) as $val) {
+            $datos_sales .= '<p>'.$val.'</p>';
+        }
+        foreach (explode(",", $promociones[0]->Contactos_sales) as $dat) {
+            $datos_bu .= '<p>'.$dat.'</p>';
+        }
+        foreach ($promociones as $key) {
+            $html .= '<div class="mdl-card mdl-card-promocion">
+                        <div class="mdl-header">
+                            <h2>'.$key->Titulo.'</h2>
+                            <p>Valid until April 30</p>
+                        </div>
+                        <div class="mdl-contenido">
+                            <div class="contenido">
+                                <div class="promocion">
+                                    <h2 class="title">Commercial Objective</h2>
+                                    <p>'.$key->Objetivo_comercial.'</p>
+                                </div>
+                                <div class="promocion">
+                                    <h2 class="title">What’s New!</h2>
+                                    <p>'.$key->Noticia.'</p>
+                                </div>
+                                <div class="promocion">
+                                    <h2 class="title">Countries that apply</h2>
+                                    <p>'.$key->Ciudades.'</p>
+                                </div>
+                                <div class="promocion">
+                                    <h2 class="title">Contacts</h2>
+                                    <p>Sales</p>
+                                    '.$datos_sales.'
+                                    </br>
+                                    <p>BU:</p>
+                                    '.$datos_bu.'
+                                </div>
+                                <div class="promocion">
+                                    <h2 class="title">Conditions</h2>
+                                    <p>Includes special prices for Pointnext attach
+                                    services (local deal)
+                                    Some promotions do not apply to all Distributors.
+                                    Please review the terms and conditions of this
+                                    promotion in pComm.</p>
+                                </div>
+                            </div>
+                            <div class="imagenes text-center">
+                                <img class="imagen-promocion" src="'.RUTA_IMG.'promo/promo1.png">
+                                <img class="imagen-promocion" src="'.RUTA_IMG.'promo/promo1.png">
+                                <img class="imagen-promocion" src="'.RUTA_IMG.'promo/promo1.png">
+                                <div class="footer-promocion">
+                                    <img src="'.RUTA_IMG.'promo/sellers.png">
+                                    <img src="'.RUTA_IMG.'promo/iquote.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+            $cont++;
+        }
+        $data['promociones'] = $html;
 		$this->load->view('v_promo', $data);
 	}
 }
