2375e4d4cb6c77e473b1ad2fc43c30404ade2170
joseminaya15 <jose.minayac15@gmail.com>
2018-03-08 09:46:52 -0500

    listado

--- public/js/login.js
+++ public/js/login.js
@@ -20,7 +20,7 @@ function ingresar(){
         if(data.error == 0){
         	$('#usuario').val("");
         	$('#password').val("");
-          location.href = 'Formulario';
+          location.href = 'Listado';
         }else {
           if(data.pass == null || data.pass == '') {
             msj('error', 'alguno de sus datos son incorrectos');
