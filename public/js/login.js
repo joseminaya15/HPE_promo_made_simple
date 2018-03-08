6f1f8635f48ac871c34feee4b297ea87ca98c4d9
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-07 19:02:31 -0500

    cerrar cesion

--- public/js/login.js
+++ public/js/login.js
@@ -78,3 +78,20 @@ function verificarDatos(e){
 		ingresar();
     }
 }
+function cerrarCesion(){
+  $.ajax({
+    url  : 'Admin/cerrarCesion',
+    type : 'POST'
+  }).done(function(data){
+    try{
+        data = JSON.parse(data);
+        if(data.error == 0){
+          location.href = 'Login';
+        }else {
+          return;
+        }
+      }catch(err){
+        msj('error',err.message);
+      }
+  });
+}
