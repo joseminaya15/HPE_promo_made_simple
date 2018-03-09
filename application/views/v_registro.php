ab7cfd18c14d512d59b71c130eb0d73f7917a0c5
joseminaya15 <jose.minayac15@gmail.com>
2018-03-09 15:26:37 -0500

    registro

---
+++ application/views/v_registro.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+        <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="description"            content="Registro de Oportunidades DCN">
+        <meta name="keywords"               content="Registro de Oportunidades DCN">
+        <meta name="robots"                 content="Index,Follow">
+        <meta name="date"                   content="Febrero 15, 2018"/>
+        <meta name="language"               content="es">
+        <meta name="theme-color"            content="#000000">
+    	<title>HPE promo made simple</title>
+    	<link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.png">
+    	<link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>font-awesome.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>roboto.css?v=<?php echo time();?>">
+    	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
+    	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
+    </head>
+    <body>
+        <section id="promo" class="section">
+            <div class="mdl-container">
+                <div class="col-xs-12 header">
+                    <div class="col-xs-6 text-left p-0">
+                        <img src="<?php echo RUTA_IMG?>logo/logo_header.png">
+                    </div>
+                    <div class="col-xs-6 text-right p-0">
+                        <p>Promos Made Simple</p>
+                    </div>
+                </div>
+                <div class="mdl-card mdl-card-registro">
+                    <div class="mdl-card__title">
+                        <h2>Register</h2>
+                    </div>
+                    <div class="mdl-card__supporting-text">
+                         
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>login.js?v=<?php echo time();?>"></script>
+    </body>
+</html>
