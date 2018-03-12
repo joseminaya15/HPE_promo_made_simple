4fcd92409a5bbf498b5e10970ecf331846b5dbf5
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-12 16:14:45 -0500

    cambios en el formulario

--- application/controllers/Formulario.php
+++ application/controllers/Formulario.php
@@ -56,11 +56,8 @@ class Formulario extends CI_Controller {
             $bu                 = $this->input->post('bu');
             $ciudades           = $this->input->post('ciudades');
             $condiciones        = $this->input->post('condiciones');
-            //$imagen             = $this->input->post('imagen');
+            $imagen             = $this->input->post('imagen');
             $deal_number        = $this->input->post('deal_number');
-            $type=$_FILES['img_up']['type'];
-            $tmp_name = $_FILES['img_up']["tmp_name"];
-            $name = $_FILES['img_up']["name"];
 
             $arrayInsert = array('Tipo'               => $tipo,
                                  /*'Codigo'             => $codigo,*/
@@ -69,7 +66,7 @@ class Formulario extends CI_Controller {
                                  'objetivo_comercial' => $objetivo_comercial,
                                  'Noticia'            => $noticia,
                                  'Condiciones'        => $condiciones,
-                                 'Imagen'             => $tmp_name,
+                                 'Imagen'             => $imagen,
                                  'Contactos_sales'    => $sales,
                                  'Contactos_BU'       => $bu,
                                  'Deal_number'        => $deal_number);
