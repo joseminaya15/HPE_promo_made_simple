39ef1f9d89f14050c99f7d08847fd6b56cec50c5
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-04-02 10:26:04 -0500

    cambios en el recuperar contraseña

--- application/controllers/Recuperar.php
+++ application/controllers/Recuperar.php
@@ -26,6 +26,7 @@ class Recuperar extends CI_Controller {
             if(count($username) == 0){
                 $data['msj'] = 'No se encuentra registrado';
             }else {
+                //$this->sendGmail($usuario, base64_decode($username[0]->pass));
                 $data['msj'] = 'Se le envió un email con sus datos';
                 //enviar Email
                 $data['error'] = EXIT_SUCCESS;
@@ -34,5 +35,32 @@ class Recuperar extends CI_Controller {
            $data['msj'] = $e->getMessage();
         }
         echo json_encode($data);
+    }
+
+    function sendGmail($email, $pass){
+      $data['error'] = EXIT_ERROR;
+      $data['msj']   = null;
+      try {  
+       $this->load->library("email");
+       $configGmail = array('protocol'  => 'smtp',
+                            'smtp_host' => 'smtpout.secureserver.net',
+                            'smtp_port' => 3535,
+                            'smtp_user' => 'confirmaciones@merino.com.pe',
+                            'smtp_pass' => 'cFm$17Pe',
+                            'mailtype'  => 'html',
+                            'charset'   => 'utf-8',
+                            'newline'   => "\r\n");
+       $this->email->initialize($configGmail);
+       $this->email->from('info@sap-latam.com');
+       $this->email->to($email);
+       $this->email->subject('');
+       $texto = '';
+       $this->email->message($texto);
+       $this->email->send();
+       $data['error'] = EXIT_SUCCESS;
+      }catch (Exception $e){
+        $data['msj'] = $e->getMessage();
+      }
+      return json_encode(array_map('utf8_encode', $data));
     }
 }
