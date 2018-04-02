4650a84a416d8df0de7e15c9f2281604c8af8d7f
joseminaya15 <jose.minayac15@gmail.com>
2018-04-02 15:05:57 -0500

    v dist

--- application/views/v_distis.php
+++ application/views/v_distis.php
@@ -41,7 +41,7 @@
             </div>
             <div class="mdl-container">
                 <div class="promociones">
-                    <div>
+                    <div class="header-search">
                         <div class="col-xs-12 buscador">
                             <div class="form-group search">
                                 <input type="email" class="form-control" id="buscador" placeholder="Search" onkeyup="buscarPromocion(event);">
