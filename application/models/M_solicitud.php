5760b455ff743e3d23f42b3c2a639e85319d1fc6
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-04-18 12:07:50 -0500

    cambios en el modelo de solicitud

--- application/models/M_solicitud.php
+++ application/models/M_solicitud.php
@@ -5,72 +5,72 @@ class M_solicitud extends  CI_Model{
         parent::__construct();
     }
     function insertarDatos($arrayInsert, $tabla){
-        $this->db->insert($tabla, $arrayInsert);
-        $sol = $this->db->insert_id();
-        if($this->db->affected_rows() != 1) {
-            throw new Exception('Error al insertar');
-            $data['error'] = EXIT_ERROR;
-        }
-        return array("error" => EXIT_SUCCESS, "msj" => MSJ_INS, "Id" => $sol);
+      $this->db->insert($tabla, $arrayInsert);
+      $sol = $this->db->insert_id();
+      if($this->db->affected_rows() != 1){
+          throw new Exception('Error al insertar');
+          $data['error'] = EXIT_ERROR;
+      }
+      return array("error" => EXIT_SUCCESS, "msj" => MSJ_INS, "Id" => $sol);
     }
     function updateDatos($arrayData, $id, $tabla){
-        $this->db->where('Id'  , $id);
-        $this->db->update($tabla, $arrayData);
-        if ($this->db->trans_status() == false) {
-            throw new Exception('No se pudo actualizar los datos');
-        }
-        return array('error' => EXIT_SUCCESS,'msj' => MSJ_UPT);
+      $this->db->where('Id'  , $id);
+      $this->db->update($tabla, $arrayData);
+      if ($this->db->trans_status() == false){
+          throw new Exception('No se pudo actualizar los datos');
+      }
+      return array('error' => EXIT_SUCCESS,'msj' => MSJ_UPT);
     }
     function deleteDatos($id_dato){
-        $sql = "DELETE FROM cards WHERE Id = ?";
-        $result = $this->db->query($sql, array($id_dato));
-        return $result;
+      $sql = "DELETE FROM cards WHERE Id = ?";
+      $result = $this->db->query($sql, array($id_dato));
+      return $result;
     }
     function verificarUsuario($user){
-        $sql = "SELECT *
-                  FROM users
-                 WHERE Email LIKE '%".$user."%'";
-        $result = $this->db->query($sql);
-        return $result->result();
+      $sql = "SELECT *
+                FROM users
+               WHERE Email LIKE '%".$user."%'";
+      $result = $this->db->query($sql);
+      return $result->result();
     }
     function getPromociones($Q, $anio){
-        $sql = "SELECT c.*,
-                       DATE_FORMAT(c.Fecha, '%d/%m/%Y') AS fecha_vencimiento,
-                       DATE_FORMAT(c.fecha_ini, '%d/%m/%Y') AS fecha_inicio
-                  FROM cards c
-                  WHERE SUBSTRING(c.Codigo, 2, 1) = ?
-                    AND SUBSTRING(c.Codigo, 6, 2) = ?
-              ORDER BY c.Fecha, c.Tipo_distribuidor, c.Last_units, c.Tipo ASC";
-        $result = $this->db->query($sql, array($Q, $anio));
-        return $result->result();
+      $sql = "SELECT c.*,
+                     DATE_FORMAT(c.Fecha, '%d/%m/%Y') AS fecha_vencimiento,
+                     DATE_FORMAT(c.fecha_ini, '%d/%m/%Y') AS fecha_inicio
+                FROM cards c
+                WHERE SUBSTRING(c.Codigo, 2, 1) = ?
+                  AND SUBSTRING(c.Codigo, 6, 2) = ?
+            ORDER BY c.Fecha, c.Tipo_distribuidor, c.Last_units, c.Tipo ASC";
+      $result = $this->db->query($sql, array($Q, $anio));
+      return $result->result();
     }
     function getPromocionesSellers(){
-        $sql = "SELECT c.*,
-                       DATE_FORMAT(c.Fecha, '%Y-%m-%d') AS fecha_vencimiento 
-                  FROM cards c
-                 WHERE c.Tipo_distribuidor = 'Resellers'
-              ORDER BY c.Last_units, c.Tipo DESC";
-        $result = $this->db->query($sql);
-        return $result->result();
+      $sql = "SELECT c.*,
+                     DATE_FORMAT(c.Fecha, '%Y-%m-%d') AS fecha_vencimiento 
+                FROM cards c
+               WHERE c.Tipo_distribuidor = 'Resellers'
+            ORDER BY c.Last_units, c.Tipo DESC";
+      $result = $this->db->query($sql);
+      return $result->result();
     }
     function getPromocionesDistis(){
-        $sql = "SELECT c.*,
-                       DATE_FORMAT(c.Fecha, '%Y-%m-%d') AS fecha_vencimiento 
-                  FROM cards c
-                 WHERE c.Tipo_distribuidor = 'Distis'
-              ORDER BY c.Last_units, c.Tipo DESC";
-        $result = $this->db->query($sql);
-        return $result->result();
+      $sql = "SELECT c.*,
+                     DATE_FORMAT(c.Fecha, '%Y-%m-%d') AS fecha_vencimiento 
+                FROM cards c
+               WHERE c.Tipo_distribuidor = 'Distis'
+            ORDER BY c.Last_units, c.Tipo DESC";
+      $result = $this->db->query($sql);
+      return $result->result();
     }
     function getPromocionesById($id){
-        $sql = "SELECT c.*,
-                       DATE_FORMAT(c.Fecha, '%d/%m/%Y') AS fecha_vencimiento,
-                       DATE_FORMAT(c.fecha_ini, '%d/%m/%Y') AS fecha_ini 
-                  FROM cards c
-                 WHERE c.Id = ?
-                ORDER BY c.Last_units, c.Tipo DESC";
-        $result = $this->db->query($sql, array($id));
-        return $result->result();
+      $sql = "SELECT c.*,
+                     DATE_FORMAT(c.Fecha, '%d/%m/%Y') AS fecha_vencimiento,
+                     DATE_FORMAT(c.fecha_ini, '%d/%m/%Y') AS fecha_ini 
+                FROM cards c
+               WHERE c.Id = ?
+              ORDER BY c.Last_units, c.Tipo DESC";
+      $result = $this->db->query($sql, array($id));
+      return $result->result();
     }
     function buscarPromocionResellers($texto){
       $sql = "SELECT c.*,
