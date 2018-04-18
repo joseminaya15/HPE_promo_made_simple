01cb78fd1aeaed9f656e0ef818c2655a0391dcf0
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-04-18 11:46:39 -0500

    cambios en el js de listado

--- public/js/jslistado.js
+++ public/js/jslistado.js
@@ -268,14 +268,14 @@ function soloLetras(e){
     especiales     = "8-37-39-46";
     tecla_especial = false
     for(var i in especiales){
-         if(key == especiales[i]){
-             tecla_especial = true;
-             break;
-         }
-     }
-     if(letras.indexOf(tecla)==-1 && !tecla_especial){
-         return false;
-     }
+        if(key == especiales[i]){
+            tecla_especial = true;
+            break;
+        }
+    }
+    if(letras.indexOf(tecla)==-1 && !tecla_especial){
+        return false;
+    }
 }
 function valida(e){
     tecla = (document.all) ? e.keyCode : e.which;
