3e13abfc16e90fd33b2791f2269ccdb4bfabb523
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-04-19 11:50:29 -0500

    cambios en el js de categorias

--- application/views/v_categorias.php
+++ application/views/v_categorias.php
@@ -53,6 +53,7 @@
         <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
         <script src="<?php echo RUTA_JS?>Utils.js?v=<?php echo time();?>"></script>
         <script src="<?php echo RUTA_JS?>jsprincipal.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>categorias.js?v=<?php echo time();?>"></script>
         <script type="text/javascript">
             if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
                 $('select').selectpicker('mobile');
