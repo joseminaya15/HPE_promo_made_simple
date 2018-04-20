8049f9a0fd2bf224061f6a837faa0966b2dcbea6
joseminaya15 <jose.minayac15@gmail.com>
2018-04-20 15:51:56 -0500

    js

--- public/js/jscategorias.js
+++ public/js/jscategorias.js
@@ -58,3 +58,20 @@ function buscarPromo(){
 	  }
 	});
 }
+function cerrarCesion(){
+	$.ajax({
+		url  : 'Categorias/cerrarCesion',
+		type : 'POST'
+	}).done(function(data){
+		try{
+        data = JSON.parse(data);
+        if(data.error == 0){
+        	location.href = 'Login';
+        }else {
+        	return;
+        }
+      }catch(err){
+        msj('error',err.message);
+      }
+	});
+}
