ef1f9b3de7ad51fd91c1ab075eae9b45523f5fb6
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-23 12:26:32 -0500

    cambios

--- application/config/database.php
+++ application/config/database.php
@@ -75,12 +75,12 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	// 'hostname' => 'localhost',
-	// 'username' => 'root',
-	// 'password' => '',
-	'hostname' => 'mysql.it-merino.com',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	/*'hostname' => 'mysql.it-merino.com',
 	'username' => 'brainblue',
-	'password' => 'Brain$blue18',
+	'password' => 'Brain$blue18',*/
 	'database' => 'promo_made_simple',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
