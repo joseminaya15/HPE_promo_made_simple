9380320df1df628d2744fcdb721cd49bc092bf06
joseminaya15 <jose.minayac15@gmail.com>
2018-07-16 15:49:12 -0500

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/HPE_promo_made_simple/';
+$config['base_url'] = 'http://localhost/HPE_promo_made_simple/';
 
 /*
 |--------------------------------------------------------------------------
